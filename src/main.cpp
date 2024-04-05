75ff01943409d2cb158a571536ff2e659ecef40d
petar <petar1pa1990@gmail.com>
2024-04-05 20:53:05 +0200

    Dodati Objekti

--- src/main.cpp
+++ src/main.cpp
@@ -56,8 +56,40 @@ struct ProgramState {
     bool ImGuiEnabled = false;
     Camera camera;
     bool CameraMouseMovementUpdateEnabled = true;
-    glm::vec3 backpackPosition = glm::vec3(0.0f);
-    float backpackScale = 1.0f;
+
+    glm::vec3 candle1Position = glm::vec3(1.5f, 10.79f, -2.5f);
+    float candle1Scale = 4.0f;
+
+    glm::vec3 globePostiotn = glm::vec3(1.0f, 10.2f, 2.3f);
+    float globeScale = 4.0f;
+
+    glm::vec3 potionPostiotn = glm::vec3(-1.0f, 11.9f, 2.0f);
+    float potionScale = 3.0f;
+
+    glm::vec3 bookStackPostiotn = glm::vec3(1.5f, 9.7f, -2.4f);
+    float bookStackScale = 0.04f;
+
+    glm::vec3 plantPosition = glm::vec3(-1.0f, 12.6f, -1.5f);
+    float plantScale = 0.3f;
+
+    glm::vec3 plant1Position = glm::vec3(-1.0f, 11.9f, -3.0f);
+    float plant1Scale = 4.0f;
+
+    glm::vec3 plant2Position = glm::vec3(-1.4f, 14.0f, -3.0f);
+    float plant2Scale = 0.005f;
+
+    glm::vec3 plant3Position = glm::vec3(-0.4f, 11.7f, 3.0f);
+    float plant3Scale = 4.0f;
+
+    glm::vec3 bookopenPosition = glm::vec3(1.5f, 9.0f, 0.0f);
+    float bookopenScale = 0.5f;
+
+    glm::vec3 tablePosition = glm::vec3(0, 5, 0);
+    float tableScale = 0.05f;
+
+    glm::vec3 bookcasePosition = glm::vec3(-3.1f, 6.5, 0);
+    float bookcaseScale = 0.15f;
+
     PointLight pointLight;
     ProgramState()
             : camera(glm::vec3(0.0f, 0.0f, 3.0f)) {}
@@ -136,8 +168,6 @@ int main() {
         return -1;
     }
 
-    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
-    stbi_set_flip_vertically_on_load(true);
 
     programState = new ProgramState;
     programState->LoadFromFile("resources/program_state.txt");
@@ -165,20 +195,52 @@ int main() {
 
     // load models
     // -----------
-    Model ourModel("resources/objects/backpack/backpack.obj");
-    ourModel.SetShaderTextureNamePrefix("material.");
+    Model candle1("resources/objects/candle2obj/untitled.obj");
+    candle1.SetShaderTextureNamePrefix("material.");
+
+    Model globe("resources/objects/globeobj/untitled.obj");
+    globe.SetShaderTextureNamePrefix("material.");
+
+    Model bookstack("resources/objects/bookstackobj/untitled.obj");
+    bookstack.SetShaderTextureNamePrefix("material.");
+
+    Model potion("resources/objects/potionobj/untitled.obj");
+    potion.SetShaderTextureNamePrefix("material.");
+
+    Model plant("resources/objects/plant/plant.obj");
+    plant.SetShaderTextureNamePrefix("material.");
+
+    Model plant1("resources/objects/plant1obj/untitled.obj");
+    plant1.SetShaderTextureNamePrefix("material.");
+
+    Model plant2("resources/objects/plant2obj/untitled.obj");
+    plant2.SetShaderTextureNamePrefix("material.");
+
+    Model plant3("resources/objects/plant3obj/untitled.obj");
+    plant3.SetShaderTextureNamePrefix("material.");
+
+    Model bookopen("resources/objects/openbookobj/untitled.obj");
+    bookopen.SetShaderTextureNamePrefix("material.");
+
+    Model table("resources/objects/desk/scene.gltf");
+    table.SetShaderTextureNamePrefix("material.");
+
+    Model bookcase("resources/objects/bookcaseobj/untitled.obj");
+    bookcase.SetShaderTextureNamePrefix("material.");
+
+    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    stbi_set_flip_vertically_on_load(true);
+
 
     PointLight& pointLight = programState->pointLight;
-    pointLight.position = glm::vec3(4.0f, 4.0, 0.0);
+    pointLight.position = glm::vec3(8.0f, 13.0f, 1.0f);
     pointLight.ambient = glm::vec3(0.1, 0.1, 0.1);
     pointLight.diffuse = glm::vec3(0.6, 0.6, 0.6);
     pointLight.specular = glm::vec3(1.0, 1.0, 1.0);
 
     pointLight.constant = 1.0f;
-    pointLight.linear = 0.09f;
-    pointLight.quadratic = 0.032f;
-
-
+    pointLight.linear = 1.0f;
+    pointLight.quadratic = 1.0f;
 
     // draw in wireframe
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -196,7 +258,6 @@ int main() {
         // -----
         processInput(window);
 
-
         // render
         // ------
         glClearColor(programState->clearColor.r, programState->clearColor.g, programState->clearColor.b, 1.0f);
@@ -204,8 +265,8 @@ int main() {
 
         // don't forget to enable shader before setting uniforms
         ourShader.use();
-        pointLight.position = glm::vec3(4.0 * cos(currentFrame), 4.0f, 4.0 * sin(currentFrame));
-        ourShader.setVec3("pointLight.position", pointLight.position);
+        pointLight.position = glm::vec3(8.0f, 13.0f, 1.0f);
+        ourShader.setVec3("pointLight.position", programState->camera.Position);
         ourShader.setVec3("pointLight.ambient", pointLight.ambient);
         ourShader.setVec3("pointLight.diffuse", pointLight.diffuse);
         ourShader.setVec3("pointLight.specular", pointLight.specular);
@@ -223,15 +284,124 @@ int main() {
 
         // render the loaded model
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model,
-                               programState->backpackPosition); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(programState->backpackScale));    // it's a bit too big for our scene, so scale it down
+        model = glm::translate(model,programState->bookopenPosition); // translate it down so it's at the center of the scene
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0, 1, 0));
+        model = glm::scale(model, glm::vec3(programState->bookopenScale));    // it's a bit too big for our scene, so scale it down
         ourShader.setMat4("model", model);
-        ourModel.Draw(ourShader);
+        bookopen.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, programState->globePostiotn);
+        model = glm::rotate(model, glm::radians(-120.0f), glm::vec3(0, 1, 0));
+        model = glm::scale(model, glm::vec3(programState->globeScale));
+        ourShader.setMat4("model", model);
+        globe.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, programState->bookStackPostiotn);
+        model = glm::rotate(model, glm::radians(130.0f), glm::vec3(0, 1, 0));
+        model = glm::scale(model, glm::vec3(programState->bookStackScale));
+        ourShader.setMat4("model", model);
+        bookstack.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, programState->potionPostiotn);
+        model = glm::rotate(model, glm::radians(30.0f), glm::vec3(0, 1, 0));
+        model = glm::scale(model, glm::vec3(programState->potionScale));
+        ourShader.setMat4("model", model);
+        potion.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, programState->plantPosition);
+        model = glm::rotate(model, glm::radians(0.0f), glm::vec3(0, 1, 0));
+        model = glm::scale(model, glm::vec3(programState->plantScale));
+        ourShader.setMat4("model", model);
+        plant.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, programState->plant1Position);
+        model = glm::rotate(model, glm::radians(-120.0f), glm::vec3(0, 1, 0));
+        model = glm::scale(model, glm::vec3(programState->plant1Scale));
+        ourShader.setMat4("model", model);
+        plant1.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.4f, 11.9f, -3.0f));
+        //model = glm::rotate(model, glm::radians(-120.0f), glm::vec3(0, 1, 0));
+        model = glm::scale(model, glm::vec3(0.005f));
+        ourShader.setMat4("model", model);
+        plant2.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.4f, 14.0f, -3.0f));
+        model = glm::rotate(model, glm::radians(-20.0f), glm::vec3(1, 0, 0));
+        model = glm::scale(model, glm::vec3(0.005f));
+        ourShader.setMat4("model", model);
+        plant2.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.4f, 14.4f, -3.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1, 0, 0));
+        model = glm::scale(model, glm::vec3(0.005f));
+        ourShader.setMat4("model", model);
+        plant2.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.4f, 14.4f, -1.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1, 0, 0));
+        model = glm::scale(model, glm::vec3(0.005f));
+        ourShader.setMat4("model", model);
+        plant2.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.4f, 14.4f, 1.2f));
+        model = glm::rotate(model, glm::radians(60.0f), glm::vec3(1, 0, 0));
+        model = glm::scale(model, glm::vec3(0.005f));
+        ourShader.setMat4("model", model);
+        plant2.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.4f, 15.5f, 3.2f));
+        model = glm::rotate(model, glm::radians(0.0f), glm::vec3(1, 0, 0));
+        model = glm::scale(model, glm::vec3(0.005f));
+        ourShader.setMat4("model", model);
+        plant2.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, programState->plant3Position);
+        //model = glm::rotate(model, glm::radians(0.0f), glm::vec3(1, 0, 0));
+        model = glm::scale(model, glm::vec3(programState->plant3Scale));
+        ourShader.setMat4("model", model);
+        plant3.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model,glm::vec3(1.5f, 9.7f, 3.0f));
+        model = glm::scale(model, glm::vec3(4.0f));
+        ourShader.setMat4("model", model);
+        candle1.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model,programState->candle1Position);
+        model = glm::scale(model, glm::vec3 (3.50f));
+        ourShader.setMat4("model", model);
+        candle1.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model,programState->tablePosition);
+        model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(0, 1, 0));
+        model = glm::scale(model, glm::vec3(programState->tableScale));
+        ourShader.setMat4("model", model);
+        table.Draw(ourShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model,programState->bookcasePosition);
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0, 1, 0));
+        model = glm::scale(model, glm::vec3(programState->bookcaseScale));
+        ourShader.setMat4("model", model);
+        bookcase.Draw(ourShader);
 
         if (programState->ImGuiEnabled)
             DrawImGui(programState);
-
 
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -312,12 +482,44 @@ void DrawImGui(ProgramState *programState) {
         ImGui::Text("Hello text");
         ImGui::SliderFloat("Float slider", &f, 0.0, 1.0);
         ImGui::ColorEdit3("Background color", (float *) &programState->clearColor);
-        ImGui::DragFloat3("Backpack position", (float*)&programState->backpackPosition);
-        ImGui::DragFloat("Backpack scale", &programState->backpackScale, 0.05, 0.1, 4.0);
+
+        ImGui::DragFloat3("Candle1 position", (float*)&programState->candle1Position);
+        ImGui::DragFloat("Candle1 scale", &programState->candle1Scale, 0.05,0.1,4.0);
+
+        ImGui::DragFloat3("Globe position", (float*)&programState->globePostiotn);
+        ImGui::DragFloat("Globe scale", &programState->globeScale, 0.05,0.1,4.0);
+
+        ImGui::DragFloat3("Book Stack position", (float*)&programState->bookStackPostiotn);
+        ImGui::DragFloat("Book Stack scale", &programState->bookStackScale, 0.05,0.1,4.0);
+
+        ImGui::DragFloat3("Potion position", (float*)&programState->potionPostiotn);
+        ImGui::DragFloat("Potion scale", &programState->potionScale, 0.05,0.1,4.0);
+
+        ImGui::DragFloat3("Plant position", (float*)&programState->plantPosition);
+        ImGui::DragFloat("Plant scale", &programState->plantScale, 0.05,0.1,4.0);
+
+        ImGui::DragFloat3("Plant1 position", (float*)&programState->plant1Position);
+        ImGui::DragFloat("Plant1 scale", &programState->plant1Scale, 0.05,0.1,4.0);
+
+        ImGui::DragFloat3("Plant2 position", (float*)&programState->plant2Position);
+        ImGui::DragFloat("Plant2 scale", &programState->plant2Scale, 0.05,0.1,4.0);
+
+        ImGui::DragFloat3("Plant3 position", (float*)&programState->plant3Position);
+        ImGui::DragFloat("Plant3 scale", &programState->plant3Scale, 0.05,0.1,4.0);
+
+        ImGui::DragFloat3("Bookopen position", (float*)&programState->bookopenPosition);
+        ImGui::DragFloat("Bookopen scale", &programState->bookopenScale, 0.05, 0.1, 4.0);
+
+        ImGui::DragFloat3("Table position", (float*)&programState->tablePosition);
+        ImGui::DragFloat("Table scale", &programState->tableScale, 0.05,0.1,4.0);
+
+        ImGui::DragFloat3("Bookcase position", (float*)&programState->bookcasePosition);
+        ImGui::DragFloat("Bookcase scale", &programState->bookcaseScale, 0.05,0.1,4.0);
 
         ImGui::DragFloat("pointLight.constant", &programState->pointLight.constant, 0.05, 0.0, 1.0);
         ImGui::DragFloat("pointLight.linear", &programState->pointLight.linear, 0.05, 0.0, 1.0);
         ImGui::DragFloat("pointLight.quadratic", &programState->pointLight.quadratic, 0.05, 0.0, 1.0);
+
         ImGui::End();
     }
 
